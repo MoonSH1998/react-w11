06d915bb637854bd154bb79bc54ab0c4f9f16b4f
문성하 <81578218+MoonSH1998@users.noreply.github.com>
2023-11-24 10:27:42 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -19,12 +19,20 @@ class App extends Component {
       subject: { title: "web", sub: "World Wide Web!" },
       welcome: { title: "welcome", desc: "Hello, React", image: webImg },
       contents: [
-        { id: 1, title: "HTML", desc: "HTML is for information",
-        image: htmlImg,
-},
-        { id: 2, title: "CSS", desc: "CSS is for design" , image: cssImg },
-        { id: 3, title: "JavaScript", desc: "JS is for interactive", image: jsImg },
-        { id: 4, title: "React", desc: "React is for UI" ,image: reactImg },
+        {
+          id: 1,
+          title: "HTML",
+          desc: "HTML is for information",
+          image: htmlImg,
+        },
+        { id: 2, title: "CSS", desc: "CSS is for design", image: cssImg },
+        {
+          id: 3,
+          title: "JavaScript",
+          desc: "JS is for interactive",
+          image: jsImg,
+        },
+        { id: 4, title: "React", desc: "React is for UI", image: reactImg },
       ],
     };
   }
@@ -32,45 +40,42 @@ class App extends Component {
     var _title,
       _desc = null,
       _image = null;
-      if (this.state.mode === "welcome") {
-        _title = this.state.welcome.title;
-        _desc = this.state.welcome.desc,
-        _image = this.state.welcome.image;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      (_desc = this.state.welcome.desc), (_image = this.state.welcome.image);
+    } else if (this.state.mode === "read") {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          _image = data.image;
 
-      } else if (this.state.mode === "read") {
-        var i = 0;
-        while (i < this.state.contents.length) {
-          var data = this.state.contents[i];
-          if (data.id === this.state.selected_content_id) {
-            _title = data.title;
-            _desc = data.desc;
-            _image = data.image;
-
-            break;
-          }
-          i = i + 1;
+          break;
         }
+        i = i + 1;
       }
-      return (
-        <div className="App">
-          <Subject
-            title={this.state.subject.title}
-            sub={this.state.subject.sub}
-            onChangePage={function () {
-              this.setState({ mode: "welcome" });
-            }.bind(this)}
-          ></Subject>
-          <TOC
-            data={this.state.contents}
-            onChangePage={function (id) {
-              this.setState({ mode: "read", selected_content_id: Number(id) });
-            }.bind(this)}
-          ></TOC>
-          <Content title={_title} desc={_desc} img={_image}></Content>
-        </div>
-      );
     }
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: "welcome" });
+          }.bind(this)}
+        ></Subject>
+        <TOC
+          data={this.state.contents}
+          onChangePage={function (id) {
+            this.setState({ mode: "read", selected_content_id: Number(id) });
+          }.bind(this)}
+        ></TOC>
+        <Content title={_title} desc={_desc} img={_image}></Content>
+      </div>
+    );
   }
-  
-  export default App;
-  
+}
+
+export default App;
